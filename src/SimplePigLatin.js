8d05a114c511cb0656070c282946223c9cf07b28
smallmonsters <34727387+smallmonsters@users.noreply.github.com>
2020-03-23 11:04:40 +0800

    completion SimplePigLatin

---
+++ src/SimplePigLatin.js
@@ -0,0 +1,31 @@
+/**
+ * 把每个单词的第一个字母移到它的末尾，然后在单词的末尾加上“ay”。不要动标点符号。
+ * 
+ * examples
+ *     pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+ *     pigIt('Hello world !');     // elloHay orldway !
+ * */
+
+
+
+//  my answer
+function pigIt(str) {
+  return str.split(" ").map(v => {
+    const first = v[0]
+    if (/[^A-z]/.test(v)) return " "+v
+    return v.replace(v[0], " ") + first + "ay"
+  }).join("").trim()
+}
+
+
+// better
+function pigIt(str) {
+  return str.replace(/\w+/g, (w) => {
+    return w.slice(1) + w[0] + 'ay';
+  });
+}
+// Best Practices
+function pigIt(str){
+  return str.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3")
+}
+
