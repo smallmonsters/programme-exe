de889dfa0b60320e4bf8435d7004eaba9166ad0a
smallmonsters <34727387+smallmonsters@users.noreply.github.com>
2020-04-01 09:44:22 +0800

    completion PathFinder#1

--- 4kyu/PathFinder#1.js
+++ 4kyu/PathFinder#1.js
@@ -26,85 +26,19 @@ const maze = `....W.
 ......`
 
 
-function Maze(maze) {
-  // 二维地图
-  this.maze = []
-  // 地图边界
-  this.boundary = [0, 0]
-  // 当前坐标
-  this.x = 0
-  this.y = 0
-  // 线路
-  this.circuit = new Map()
-  this.init = () => {
-    this.circuit.set(this.x + this.y, {
-      up: true,
-      left: true,
-      right: false,
-      donw: false,
-    })
-    maze.split(/\n/).forEach((v, i) => {
-      if (v !== "") {
-        if (!this.maze[i]) this.maze[i] = []
-        this.maze[i] = v.split("")
-      }
-    });
-    this.boundary[0] = this.maze.length
-    this.boundary[1] = this.maze[0].length
-  }
-  this.moveL = () => this.y = this.y - 1
-  this.moveD = () => this.x = this.x + 1
-  this.moveR = () => this.y = this.y + 1
-  this.moveU = () => this.y = this.y - 1
-  this.hasWalk = () => {
-    const down = this.x + 1 === this.boundary[0] || this.maze[this.x][this.y + 1] === "W"
-    const left = this.y === 0 || this.maze[this.x][this.y - 1] === "W"
-    const up = this.x === 0 || this.maze[this.x - 1][this.y] === "W"
-    const right = this.y + 1 === this.boundary[1] || this.maze[this.x][this.y + 1] === "W"
-    return left || right || down || up
-  }
-  // 判断一个点是否可以移动
-  hasExit = (point, to) => {
-    if (this.circuit.has(point)) {
-      const { left, up, down, right } = this.circuit.get(point)
-      return left, up, down, right
-    } else {
-      this.circuit.set(point, {
-        up: to === "up",
-        left: to === "left",
-        right: to === "right",
-        down: to === "down",
-      })
-    }
-  }
-  // 接受一个移动方向,规定先向下移动，然后逆时针旋转，既下=》右=》上=》左
-  this.start = (to = "down") => {
-    if (this.y + 1 === this.boundary[1] && this.x + 1 === this.boundary[0]) return true
-    const point = this.x + this.y
-    if (this.hasExit(point)) {
 
-    }
-    // if (this.x + 1 < this.boundary[0] && to === "down") {
-    //   this.moveD()
-    //   if (this.hasWalk()) return this.start("left")
-    //   return false
-    // }
-    // if (this.y + 1 < this.boundary[1] && to === "right") {
-    //   this.moveR()
-    //   if (this.hasWalk()) return this.start("up")
-    //   return false
-    // }
-    // if (this.y - 1 >= 0 && this.y === this.boundary[1] && to === "left") {
-    //   this.moveL()
-    //   if (this.hasWalk()) return this.start()
-    //   return false
-    // }
-  }
-  this.init()
-}
 function pathFinder(maze) {
-  let mazes = new Maze(maze)
-  console.log(mazes.start())
-  return mazes.start()
+  maze = maze.split('\n').map(r => {
+    return [...r]
+  });
+  var len = maze.length, stack = [[0, 0]];
+  while (stack.length) {
+    let [x, y] = stack.pop();
+    let a = maze[y][x]
+    if (maze[y][x] !== '.') continue;
+    maze[y][x] = 'X';
+    [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]].filter(([i, j]) => i >= 0 && j >= 0 && i < len && j < len).forEach(([i, j]) => stack.push([i, j]));
+  }
+  return maze[len - 1][len - 1] === 'X';
 }
 pathFinder(maze)
