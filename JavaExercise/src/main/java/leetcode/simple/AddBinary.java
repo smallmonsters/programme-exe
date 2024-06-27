4c2d4c9c72e32b40fbd58c151a3dfcf9f303a5db
梁普榉 <34727387+smallmonsters@users.noreply.github.com>
2024-06-27 09:23:28 +0800

    提交

---
+++ JavaExercise/src/main/java/leetcode/simple/AddBinary.java
@@ -0,0 +1,21 @@
+package leetcode.simple;
+
+public class AddBinary {
+
+  public static String addBinary(String a, String b) {
+    int bb = a.charAt(0) - '0';
+    System.out.println();
+    System.out.println('1' + '1');
+    return "100";
+//    if (!a.isEmpty() && !b.isEmpty()) {
+//      String[] aArr = a.split("");
+//      String[] bArr = b.split("");
+//      int index = 1;
+//      while (index <= aArr.length || index <= bArr.length) {
+//        aArr[aArr.length - index] + bArr[bArr.length - index];
+//      }
+//    } else {
+//      return a.isEmpty() ? b : a;
+//    }
+  }
+}
---
+++ JavaExercise/src/test/leetcode/simple/AddBinaryTest.java
@@ -0,0 +1,14 @@
+package leetcode.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AddBinaryTest {
+  @Test
+  void addBinary() {
+    assertEquals("100", AddBinary.addBinary("1a1", "1"));
+//    assertArrayEquals(new int[]{1, 0}, PlusOne.plusOne(new int[]{9}));
+//    assertArrayEquals(new int[]{1, 1}, PlusOne.plusOne(new int[]{1, 0}));
+  }
+}
